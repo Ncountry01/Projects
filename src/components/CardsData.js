5892f04befac07e0089a17197667ee7bdc4a0a35
Niradity Emmanuel <yadavrjking66997@gmail.com>
2022-04-18 16:36:28 +0530

    ShoppingApp

---
+++ src/components/CardsData.js
@@ -0,0 +1,83 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "T- cotton-Shirt",
+        imgdata: "https://cdn.pixabay.com/photo/2017/01/13/04/56/blank-1976334_960_720.png",
+        address: "North Indian,",
+        delimg: "https://cdn.pixabay.com/photo/2017/01/13/04/56/blank-1976334_960_720.png",
+        somedata: " 1175 + order placed from here recently",
+        description:"Pure cotton and comfortable to wearing in all seasonally",
+        price: 350,
+        rating: "3.8",
+        arrimg: "https://cdn.pixabay.com/photo/2017/01/13/04/56/blank-1976334_960_720.png",
+        qnty:0
+    },
+    {
+        id: 2,
+        rname: "Cotton-T-Shirt",
+        imgdata: "https://cdn.pixabay.com/photo/2016/11/23/06/57/isolated-t-shirt-1852114_960_720.png",
+        address: "North India",
+        delimg: "https://cdn.pixabay.com/photo/2016/11/23/06/57/isolated-t-shirt-1852114_960_720.png",
+        somedata: " 2525 + order placed from here recently",
+        description:"Pure cotton and comfortable to wearing in all seasonally",
+        price: 25,
+        rating: "3.9",
+        arrimg: "https://cdn.pixabay.com/photo/2016/11/23/06/57/isolated-t-shirt-1852114_960_720.png",
+         qnty:0
+    },
+    {
+        id: 3,
+        rname: "Cotton T-Shirt",
+        imgdata: "https://cdn.pixabay.com/photo/2016/11/23/06/57/isolated-t-shirt-1852113_960_720.png",
+        address: "North India",
+        delimg: "https://cdn.pixabay.com/photo/2016/11/23/06/57/isolated-t-shirt-1852113_960_720.png",
+        somedata: " 650 + order placed from here recently",
+        description:"Pure cotton and comfortable to wearing in all seasonally",
+        price: 70,
+        rating: "4.2",
+        arrimg: "https://cdn.pixabay.com/photo/2016/11/23/06/57/isolated-t-shirt-1852113_960_720.png",
+         qnty:0
+    },
+    {
+        id: 4,
+        rname: "T-Shirt",
+        imgdata: "https://rukminim2.flixcart.com/image/309/371/kiyw9e80-0/t-shirt/z/s/j/m-t2-seven-rocks-original-imafyn68m9v2pvwh.jpeg?q=50",
+        address: "North India",
+        delimg: "https://rukminim2.flixcart.com/image/309/371/kiyw9e80-0/t-shirt/z/s/j/m-t2-seven-rocks-original-imafyn68m9v2pvwh.jpeg?q=50",
+        somedata: " 300 + order placed from here recently",
+        description:"Pure cotton and comfortable to wearing in all seasonally",
+        price: 70,
+        rating: "3.8",
+        arrimg: "https://rukminim2.flixcart.com/image/309/371/kiyw9e80-0/t-shirt/z/s/j/m-t2-seven-rocks-original-imafyn68m9v2pvwh.jpeg?q=50",
+         qnty:0
+    },
+    {
+        id: 5,
+        rname: "Cotton -T-Shirt",
+        imgdata: "https://m.media-amazon.com/images/I/61100SqRbcL._AC_UL320_.jpg",
+        address: "North Indian",
+        delimg: "https://m.media-amazon.com/images/I/61100SqRbcL._AC_UL320_.jpg",
+        somedata: "1050 + order placed from here recently",
+          description:"Pure cotton and comfortable to wearing in all seasonally",
+        price: 210,
+        rating: "4.0",
+        arrimg: "https://m.media-amazon.com/images/I/61100SqRbcL._AC_UL320_.jpg",
+         qnty:0
+    },
+    {
+        id: 6,
+        rname: "Cotton -T-shirt",
+        imgdata: "https://m.media-amazon.com/images/I/51mr7N7d0oL._AC_UL320_.jpg",
+        address: "North India",
+        delimg: "https://m.media-amazon.com/images/I/51mr7N7d0oL._AC_UL320_.jpg",
+        somedata: " 1100 + order placed from here recently",
+          description:"Pure cotton and comfortable to wearing in all seasonally",
+        price: 100,
+        rating: "3.8",
+        arrimg: "https://m.media-amazon.com/images/I/51mr7N7d0oL._AC_UL320_.jpg",
+         qnty:0
+    },
+    
+];
+
+export default Cardsdata;
---
+++ src/components/CardsDetails.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from 'react'
+import Table from 'react-bootstrap/Table'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { DLT,ADD,REMOVE } from '../redux/actions/action'
+import { Button } from 'react-bootstrap'
+import RemoveCircleOutlineIcon  from '@mui/icons-material/RemoveCircleOutline';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+
+
+
+const CardsDetails = () => {
+
+  const [data,setData] = useState([]);
+  // console.log(data);
+
+  const {id} = useParams();
+  // console.log(id);
+
+  const history = useNavigate();
+
+  const dispatch = useDispatch();
+
+  
+  const getdata = useSelector((state)=> state.cartreducer.carts);
+  // console.log(getdata);
+
+
+  const compare = ()=>{
+    let comparedata = getdata.filter((e)=>{
+      return e.id == id
+    });
+    setData(comparedata);
+  }
+
+  // add data
+  
+
+  const send = (e)=>{
+    // console.log(e);
+    dispatch(ADD(e));
+  }
+  
+  const dlt = (id)=>{
+    dispatch(DLT(id));
+    history("/");
+}
+
+// remove one
+const remove = (item)=>{
+  dispatch(REMOVE(item))
+}
+
+
+  useEffect(()=>{
+    compare();
+  },[id])
+
+  return (
+    <>
+      <div className="container mt-2">
+        <h2 className='text-center'>Iteams Details Page
+        </h2>
+
+        <section className='container mt-3'>
+          <div className="iteamsdetails">
+          {
+            data.map((ele)=>{
+              return (
+                <>
+                <div className="items_img">
+              <img src={ele.imgdata} alt="" />
+            </div>
+
+            <div className="details">
+              <Table>
+                <tr>
+                  <td>
+                    <p> <strong>Clothes</strong>  : {ele.rname}</p>
+                    <p> <strong>Price</strong>  : ₹{ele.price}</p>
+                    <p> <strong>Type</strong>  : {ele.address}</p>
+                    <p> <strong>Total</strong>  :₹  {ele.price * ele.qnty}</p>
+                    <div className='mt-5 d-flex justify-content-between align-items-center' style={{width:100,cursor:"pointer",background:"#ddd",color:"#111"}}>
+                    <span style={{fontSize:24}} onClick={()=>send(ele)}>
+                    <AddCircleOutlineIcon style={{width:"2.7rem", height:"2.7rem", color:"green"}}/> 
+                    </span>
+                    <span style={{fontSize:22}} > <Button style={{backgroundColor:"white", color:"black", width:"3rem"}}>{ele.qnty}</Button></span>
+                    <span style={{fontSize:24}} onClick={ele.qnty <=1 ? ()=>dlt(ele.id) : ()=>remove(ele)}>
+                   <RemoveCircleOutlineIcon style={{width:"2.7rem", height:"2.7rem", color:"red"}}/>           
+                    </span> 
+
+                    </div>
+
+                  </td>
+                  <td>
+                    <p><strong>Rating :</strong> <span style={{background:"green",color:"#fff",padding:"2px 5px",borderRadius:"5px"}}>{ele.rating} ★	</span></p>
+                    <p style={{padding:"2%"}}><strong>Order Review :</strong> <span >{ele.somedata}	</span></p>
+                    <p><strong></strong> <span ><i className='fas fa-trash' onClick={()=>dlt(ele.id)} style={{color:"red",fontSize:20,cursor:"pointer"}}>
+                    <Button style={{backgroundColor:"red", color:"white"}}>Remove</Button>
+                    </i>	</span></p><br/><br/><br/>
+                    <Button variant="warning" size="lg" style={{backgroundColor:"orange", color:"white"}}>PLACE ORDER</Button>
+                  </td>
+                </tr>
+              </Table>
+            </div>
+          
+                </>
+              )
+            })
+          }
+          </div>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default CardsDetails
